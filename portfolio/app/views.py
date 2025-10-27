4fafdc228365b7b44137f7016a81d3363e7e76ce
ap2498 <adityapaul2498@gmail.com>
2025-10-27 11:53:45 +0530

    changes

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from app.models import Project, ProjectImages, Category
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def entry(request):
     return render(request,'entry.html')
-
+@login_required(login_url='/load_login/')
 def load_create_project(request):
     return render(request,'create.html')
 def load_home_page(request):
     return render(request,'home.html')
-
+def load_single_project_page(request):
+    return render(request,'single_project.html')
+def load_category_page(request):
+    return render(request,'category.html')
+def load_login(request):
+    return render(request,"login.html")
 def create_project(request):
     if request.method=="POST":
         title=request.POST.get('title')
@@ -31,7 +40,7 @@ def create_project(request):
             'message':"Project Created Succesfully"
         })
     
-
+@csrf_exempt
 def create_category(request):
     if request.method=="POST":
         name=request.POST.get('name')
@@ -74,3 +83,130 @@ def fetch_all_projects(request):
         'status':200,
         'data':data_list
     })
+
+
+def fetch_single_project(request,pk):
+    project=Project.objects.get(id=pk)
+    data=[]
+    data.append({
+        'id':project.id,
+        'title':project.title,
+        'description':project.description,
+        'url':project.url,
+        'technology':project.technology,
+        'cover':project.cover.url,
+        # 'category':{
+        #     'id':project.category.id,
+        #     'category':project.category.name,
+        #     'project':project.category.value
+        # },
+        'images':[{
+            'id':p.id,
+            'url':p.picture.url,
+            'project':p.project.id
+        }for p in project.images.all()]
+    })
+
+    return JsonResponse({
+        'message':"Data fetched succesfully",
+        'status':200,
+        'data':data
+    })
+
+def fetch_by_category(request,id):
+    projects=Project.objects.filter(category=id)
+    data=[]
+    for project in projects:
+        data.append({
+        'id':project.id,
+        'title':project.title,
+        'description':project.description,
+        'url':project.url,
+        'technology':project.technology,
+        'cover':project.cover.url,
+        # 'category':{
+        #     'id':project.category.id,
+        #     'category':project.category.name,
+        #     'project':project.category.value
+        # },
+        'images':[{
+            'id':p.id,
+            'url':p.picture.url,
+            'project':p.project.id
+        }for p in project.images.all()]
+    })
+        
+    return JsonResponse({
+        'message':"Data fetched succesfully",
+        'status':200,
+        'data':data
+    })
+
+
+def get_categories(request):
+    categories=Category.objects.all()
+    category_list=[]
+    for cat in categories:
+        category_list.append({
+            'id':cat.id,
+            'value':cat.value,
+            'category':cat.name
+        })
+    
+    return JsonResponse({
+        'status':200,
+        'message':"Data fetched succesfully",
+        'data':category_list
+    })
+
+@csrf_exempt
+def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({
+                'status':200,
+                'message':'Username already exists'
+            })
+        user=User.objects.create_user(username=username,password=password)
+        return JsonResponse({
+            'message':"Signup succesful",
+            'status':200
+        })
+    
+@csrf_exempt
+def login_user(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user= authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            return JsonResponse({
+                'status':200,
+                'message':"Logged in Succesfully"
+            })
+        else:
+            return JsonResponse({
+                'message':"USer not found",
+                'status':400
+            })
+@csrf_exempt
+def logout_user(request):
+    if request.method == "POST":
+        logout(request)
+        return JsonResponse({
+            'status': 200,
+            'message': 'Logged out successfully',
+            'data': []
+        })
+
+    return JsonResponse({
+        'status': 405,
+        'message': 'Method not allowed. Use POST instead.',
+        'data': []
+    }, status=405)
--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -29,6 +29,16 @@ urlpatterns = [
     path('create_project/',views.create_project,name='create_project'),
     path('create_category/',views.create_category,name='create_category'),
     path('view_all_projects/',views.fetch_all_projects,name='fetch_all_projects'),
+    path('single_project/',views.load_single_project_page,name='single_project'),
+    path('view_one_project/<int:pk>/',views.fetch_single_project,name='fetch_one_project'),
+    path('fetch_by_category/<int:id>/',views.fetch_by_category,name='fetch_by_category'),
+    path('get_categories/',views.get_categories,name='get_categories'),
+    path('logout_custom/',views.logout_user,name='logout_custom'),
+    path('login_custom/',views.login_user,name='login_custom'),
+    path('signup/',views.signup,name='signup'),
+    path('load_login/',views.load_login,name='load_login'),
+
+
 
 ]
 
